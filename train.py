d28fc45a05646b87ae48e4822858990fb52309af
Sara <77743508+saraashojaeii@users.noreply.github.com>
2024-09-01 10:31:07 -0500

    Update train.py

--- train.py
+++ train.py
@@ -64,11 +64,11 @@ for epoch in range(0, epochs):
   if arg_loss == 'ourbce':
       loss_function = AdaptiveTverskyCrossEntropyWeightedLoss(2, arg_alpha, arg_beta, 4/3, 0.8, 0.2)
   if arg_loss == 'ourlc':
-      loss_function = AdaptiveTverskyCrossEntropyLcDiceWeightedLoss(2, arg_alpha, arg_beta, 4/3, 0.8, 0, 0.2)
+      loss_function = AdaptiveTverskyCrossEntropyLcDiceWeightedLoss(2, arg_alpha, arg_beta, 4/3, 0.7, 0.2, 0.1)
   if arg_loss == 'lcdice':
       loss_function = LcDiceLoss()
   if arg_loss == 'ourdistlc':
-     loss_function = AdaptiveTverskyLcDiceDistanceWeightedLoss(2, arg_alpha, arg_beta, 4/3, 1, 0, 0)
+     loss_function = AdaptiveTverskyLcDiceDistanceWeightedLoss(2, arg_alpha, arg_beta, 4/3, 0.7, 0.2, 0.1)
   
   # gap_loss_fn = GapLoss(K=3)
   # mse_loss_fn = nn.MSELoss()
