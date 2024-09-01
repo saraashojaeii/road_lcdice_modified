0bb0d2b87ee29b2fa2424f91b93f21217b967448
Sara <77743508+saraashojaeii@users.noreply.github.com>
2024-09-01 11:01:26 -0500

    Update train.py

--- train.py
+++ train.py
@@ -70,7 +70,7 @@ for epoch in range(0, epochs):
   if arg_loss == 'ourdistlc':
      loss_function = AdaptiveTverskyLcDiceDistanceWeightedLoss(2, arg_alpha, arg_beta, 4/3, 0.8, 0.2, 0)
   if arg_loss == 'gap':
-     loss_function = GapLoss(K=1)
+     loss_function = GapLosswithL2(K=1)
   
   # gap_loss_fn = GapLoss(K=3)
   # mse_loss_fn = nn.MSELoss()
